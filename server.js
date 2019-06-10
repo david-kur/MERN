667ba2642a903c2ef1ad72afd0db2c8d2d101564
David Kurniawan <david.7stars@gmail.com>
2019-06-10 12:14:35 +1200

    Add auth via JWT

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ connectDB();
 app.use(express.json({ extended: false }));
 app.get('/', (req, res) => res.send('API running'));
 app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 // server port setup
 const PORT = process.env.PORT || 5000;
