406210581b4be5bf250ea7ef5119437e7ee9dd2f
David Kurniawan <david.7stars@gmail.com>
2019-06-11 14:33:29 +1200

    Add register and login form

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -5,8 +5,7 @@ const Navbar = () => (
   <nav className="navbar bg-dark">
     <h1>
       <Link to="/">
-        <i className="fas fa-code" />
-        Dev Connector
+        <i className="fas fa-code" /> Dev Connector
       </Link>
     </h1>
     <ul>
